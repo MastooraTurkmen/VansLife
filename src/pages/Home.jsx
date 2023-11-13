1b71aa2483dff5c5565171736cc37d8ea32a19d9
MastooraTurkmen <132576850+MastooraTurkmen@users.noreply.github.com>
2023-11-13 16:49:06 +0430

    Apdating pages component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="vans">Find your van</Link>
-        </div>
-    )
-};
+  return (
+    <div className="home-container">
+      <h1>You got the travel plans, we got the travel vans.</h1>
+      <p>
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <Link to="vans">Find your van</Link>
+    </div>
+  );
+}
