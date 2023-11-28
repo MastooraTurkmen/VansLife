039932d3548d295787d5c134eb9b3236f17aca95
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-11-28 07:20:48 +0430

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,28 +8,39 @@ export default function Header() {
         color: "#161616"
     }
     
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
                 <NavLink 
-                    to="/host"
+                    to="host"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Host
                 </NavLink>
                 <NavLink 
-                    to="/about"
+                    to="about"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     About
                 </NavLink>
                 <NavLink 
-                    to="/vans"
+                    to="vans"
                     style={({isActive}) => isActive ? activeStyles : null}
                 >
                     Vans
                 </NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src="../assets/images/avatar-icon.png" 
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
